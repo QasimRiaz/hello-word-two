c4f44c96555a6ac05c71c20e47b345281f6a3ca5
QasimRiaz <qasim.riaz@e2esp.com>
2016-11-22 18:52:39 +0500

    Update hello_word.php

--- hello_word.php
+++ hello_word.php
@@ -12,6 +12,9 @@ Version: 2.0
 
 
 require_once( 'BFIGitHubPluginUploader.php' );
-if ( is_admin() ) {
+
+add_action( 'admin_init', 'myprefix_handle_updates' );
+
+function myprefix_handle_updates() {
     new BFIGitHubPluginUpdater(__FILE__, 'QasimRiaz', "hello-word-two" );
 }
