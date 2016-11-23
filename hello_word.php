586532bffecd069fac6ac2a0cae9fd0e7f578ecb
QasimRiaz <qasim.riaz@e2esp.com>
2016-11-23 11:38:13 +0500

    Update hello_word.php

--- hello_word.php
+++ hello_word.php
@@ -1,17 +1,27 @@
 <?php
+
 /**
- * @package hello-word-two
- * @version 2.0
+ * Hello Word
  *
-Plugin Name: Hello word
-Description:  Hello word
-Author: Qasim riaz
-Version: 2.0
+ * @package   Hello_Word_Two
+ * @author    Qasim Riaz
+ * @license   GPL-2.0+
+ * @link      https://github.com/QasimRiaz/hello-word-two
+ */
 
-
-*/
-
-
+/**
+ * Plugin Name:       Hello Word
+ * Plugin URI:        https://github.com/QasimRiaz/hello-word-two
+ * Description:       A plugin to automatically update GitHub.
+ * Version:           2.0
+ * Author:            Qasim Riaz
+ * License:           GNU General Public License v2
+ * Text Domain:       hello-word-two
+ * Network:           true
+ * GitHub Plugin URI: https://github.com/QasimRiaz/hello-word-two
+ * Requires WP:       4.0
+ * Requires PHP:      5.3
+ */
 
 
 
