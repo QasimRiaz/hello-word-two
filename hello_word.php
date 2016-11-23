f1e273b340dad3b7ba2503fc4cdbb1b8e4c3f255
QasimRiaz <qasim.riaz@e2esp.com>
2016-11-23 12:55:50 +0500

    Update hello_word.php

--- hello_word.php
+++ hello_word.php
@@ -38,7 +38,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
             'requires' => '3.0', // which version of WordPress does your plugin require?
             'tested' => '3.3', // which version of WordPress is your plugin tested up to?
             'readme' => 'README.md', // which file to use as the readme for the version number
-            'access_token' => '01dc60f971844c78f480a8a7647cbf8b65fc1b47', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+            'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
         );
         new WP_GitHub_Updater($config);
     }
